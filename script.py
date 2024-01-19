306968deebcc2617f5c40ffbd753e9500c8ef18c
azurekite83 <azurekite83@gmail.com>
2024-01-19 01:00:41 -0800

    Refactored some code. Continuing to add functionality

--- script.py
+++ script.py
@@ -1,56 +1,47 @@
 from urllib import request
 from http.client import HTTPResponse
 from time import sleep
+from zipfile import ZipFile
 
 import tarfile, sys, os
 
+def write_and_extract(name, response):
+    if ".zip" in name:
+        print("Writing to disk and extracting...")
+        new_file = open(name, "w+b")
+        new_file.write(response)
+                
+        new_file.close()
+                
+        zipped_file = ZipFile(name)
+        zipped_file.extractall(".")
+        zipped_file.close()
+                
+        print("Done.")
+    elif ".tar.xz" in name:
+        print("Writing to disk and extracting...")
+        
+        new_file = open(name, "w+b")
+        new_file.write(response)
+        
+        new_file.close()
+        
+        tar_file = tarfile.open(name, "r:xz")
+        tar_file.extractall(".")
+        tar_file.close()
+        
+        print("Done.")
+    else:
+        print("File format not supported.")
+    
 print("Fetching man pages...")
 
 with request.urlopen("https://mirrors.edge.kernel.org/pub/linux/docs/man-pages/man-pages-6.05.tar.xz") as response:
-    print("Finished.")
-
-    print("Reading fetched data...")
     
     file_data = HTTPResponse.read(response)
-    file_name = input("Filename? ")
-    file_name = file_name + ".tar.xz"
-
-    print("Checking if file exists...")
-    sleep(1)
-
-    if os.path.exists(file_name):
-        print("File already exists.")
-        replace_file = input("Would you like to overwrite existing file? (y/Y): ")
+    print("Done.")
     
-        if replace_file == "y" or replace_file == "Y":
-            new_file = open(file_name, "w+b")
-        
-            print("Overwriting file...")
-            sleep(1)
-            new_file.write(file_data)
-        
-            print("Done.")
-            new_file.close()
-        
-        else:
-            print("Aborting")
-            sys.exit(1)
-    else:
-        with open(file_name, "w+b") as new_file:
-            print("File does not exist. Writing data to file...")
-            sleep(1)
-            new_file.write(file_data)
-        
-            print("Done.")
-    
-
-    print("Extracting tar file...")
-    sleep(1)
-
-    with tarfile.open(file_name, "r:xz") as tar_file:
-        tar_file.extractall()
-    
-        print("Finished.")
+    write_and_extract("man-pages.tar.xz", file_data)
 
 print("Checking if requirements are satisfied...")
 sleep(1)
@@ -69,29 +60,37 @@ sub_directories = path.split(path_delimiters[operating_system])
 
 requirements_satisfied = [{"make": False}, {"man": False}]
 requirement = [list(item.keys()) for item in requirements_satisfied]
+
+#Stripping key out of inner lists
+for i in range(0, len(requirement)):
+    requirement[i] = "".join(requirement[i])
+
 pos = 0
 
-#Ew wtf have I done
+#Fixed it
 for directory in sub_directories:
-    if os.path.exists(directory + requirement[pos][pos]):
-        print(f"Found {requirement[pos][pos]}. Proceeding...")
-        requirements_satisfied[pos][requirement[pos][pos]] = True
+    if os.path.exists(directory + requirement[pos]):
+        print(f"Found {requirement[pos]}. Proceeding...")
+        requirements_satisfied[pos][requirement[pos]] = True
         pos += 1
 pos = 0
 
 for item in requirements_satisfied:
-    if item[requirement[pos][pos]] == False:
-        print(f"Could not find {requirement[pos][pos]}, finding {requirement[pos][pos]} and installing...")
+    if item[requirement[pos]] == False:
+        print(f"Could not find {requirement[pos]}, finding {requirement[pos]} and installing...")
         sleep(1)
         
-        if requirement[pos][pos] == "make":
-            with request.urlopen("https://github.com/gcc-mirror/gcc") as response:
+        if requirement[pos] == "make":
+            print("Downloading make...")
+            with request.urlopen("https://github.com/gcc-mirror/gcc/archive/master.zip") as response:
                 response_data = HTTPResponse.read(response)
-                print(response_data)
-        elif requirement[pos][pos] == "man":
-            with request.urlopen("https://man-db.nongnu.org/development.html") as response:
+                write_and_extract("gcc.zip", response_data)
+                
+        elif requirement[pos] == "man":
+            print("Downloading man-db...")
+            with request.urlopen("https://gitlab.com/man-db/man-db/-/archive/main/man-db-main.zip") as response:
                 response_data = HTTPResponse.read(response)
-                print(response_data)
+                write_and_extract("man-db.zip", response_data)
     pos += 1
     
 
